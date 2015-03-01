6f23ed5fe035fc88563895b3ce370026c764575b
Miguel Tafoya <miguel.tafoya@outlook.com>
2015-02-28 21:27:18 -0600

    Primera Practica

---
+++ 01 - Roll-a-ball - Unity/Assets/Scrip/controlPelota.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controlPelota : MonoBehaviour {
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+
+	void Start (){
+		count = 0;
+		SetCounText();
+		winText.text = "";
+	}
+	void FixedUpdate(){
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertival = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertival);
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag =="PickUp"){
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCounText();
+		}
+	}
+
+	void SetCounText(){
+		countText.text = "Count: " + count.ToString();
+		if (count >= 10) {
+			winText.text="YOU WINN!!!!";		
+		}
+	}
+
+}
